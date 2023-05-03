3bf13c98a70ccdfce91657791888fd3e2969877e
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-05-03 14:25:21 +0300

    mongodb commit

---
+++ back_end/README.md
@@ -0,0 +1,5 @@
+# sensei_mern_setup
+
+- install mongodb
+- create .env file
+- add the mongo url to the .env
--- back_end/app.js
+++ back_end/app.js
@@ -1,8 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+const cors = require('cors')
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
+const { default: mongoose } = require("mongoose");
 
 // route  paths
 var indexRouter = require('./routes/index');
@@ -21,19 +24,37 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // database
-const URL = 
+const URL = "mongodb://localhost:27017/senseimernsetup"
+
+const option = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // autoIndex: false, // Don't build indexes
+  maxPoolSize: 10,
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+}
+
+mongoose.set("strictQuery", true)
+mongoose
+  .connect(process.env.MONGO_URL || URL, this.options)// emit options on host
+  .then(() => console.log("DB connected successfully!")).catch((err) => console.error(err))
 
 // routes
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 
+// api routes
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
